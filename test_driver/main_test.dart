e9975aa4317276fb38d9acc35aa8f52d458d7f9c
ihsanbaguskpvendor <ihsan.bagus@kpvendor.id>
2023-03-25 21:38:22 +0700

    setting keystore

--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -1,3 +1,19 @@
-import 'package:integration_test/integration_test_driver.dart';
+import 'dart:io';
 
-Future<void> main() => integrationDriver();
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() async {
+  try {
+    await integrationDriver(
+      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+        final File image = await File('screenshots/$screenshotName.png')
+            .create(recursive: true);
+
+        image.writeAsBytesSync(screenshotBytes);
+        return true;
+      },
+    );
+  } catch (e) {
+    print('onScreenshot - error - $e');
+  }
+}
