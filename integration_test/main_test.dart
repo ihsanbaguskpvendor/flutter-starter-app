b652a740eba8bd92032428f0457cc4c584fc8896
ihsanbagus <bagusikhsan@gmail.com>
2023-03-25 15:06:05 +0700

    ghaction

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:fluttercicd/main.dart' as app;
 
 void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding();
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
@@ -14,6 +17,7 @@ void main() {
 
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
+      await takeScreenshot(tester, binding, 'shot-1');
 
       // Finds the floating action button to tap on.
       final Finder fab = find.byTooltip('Increment');
@@ -23,9 +27,23 @@ void main() {
 
       // Trigger a frame.
       await tester.pumpAndSettle();
+      await takeScreenshot(tester, binding, 'shot-2');
 
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
     });
   });
+}
+
+takeScreenshot(tester, binding, name) async {
+  if (Platform.isAndroid) {
+    try {
+      await binding.convertFlutterSurfaceToImage();
+    } catch (e) {
+      print("TakeScreenshot exception $e");
+    }
+    await tester.pumpAndSettle();
+  }
+
+  await binding.takeScreenshot(name);
 }
